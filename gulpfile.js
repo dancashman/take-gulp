e9eb0cdaaf5c74300c09330be1c6cb9b86277b58
Beshoy Louka <beshoy.louka@gmail.com>
2014-01-09 18:15:30 -0500

    added gulpify to perform browserify tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 var recess = require('gulp-recess');
+var browserify = require('gulpify');
+
+gulp.task('bundle', function() {
+  gulp.src('./app/js/main.js')
+    .pipe(browserify('bundle.js'))
+    .pipe(gulp.dest('app/js/'));
+});
+
+gulp.task('js', function () {
+    gulp.run('bundle');
+});
 
 gulp.task('default', function () {
     gulp.src('src/app.css')
